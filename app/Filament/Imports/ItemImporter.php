b828a9775a55da38c6e72e08b4be90d362e5287b
Lumex <54485140+LumexDE@users.noreply.github.com>
2026-05-23 17:27:59 -0400

    feat: Enhance ItemImporter with example values for import columns and improve department selection logic

--- app/Filament/Imports/ItemImporter.php
+++ app/Filament/Imports/ItemImporter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Imports;
 
 use App\Models\Item;
@@ -23,80 +25,97 @@ class ItemImporter extends Importer
             ImportColumn::make('name')
                 ->label(__('general.name'))
                 ->requiredMapping()
-                ->rules(['required', 'max:64']),
+                ->rules(['required', 'max:64'])
+                ->examples(['Beispiel Artikel 1', 'Schwerlastregal']),
 
             ImportColumn::make('description')
                 ->label(__('general.description'))
-                ->rules(['max:10000']),
+                ->rules(['max:10000'])
+                ->examples(['Dies ist eine Beschreibung für den Beispielartikel.', 'Großes Regal für das Hauptlager.']),
 
             ImportColumn::make('comment')
                 ->label(__('general.comment'))
-                ->rules(['max:100000']),
+                ->rules(['max:100000'])
+                ->examples(['Ein wichtiger Kommentar.', 'Regalplatz A-12']),
 
             // --- Tab: Details ---
             ImportColumn::make('price')
                 ->label(__('general.price'))
                 ->numeric()
-                ->rules(['nullable', 'numeric', 'min:0']),
+                ->rules(['nullable', 'numeric', 'min:0'])
+                ->examples(['19.99', '149.50']),
 
             ImportColumn::make('serialnumber')
                 ->label(__('general.serialnumber'))
-                ->rules(['nullable', 'max:250']),
+                ->rules(['nullable', 'max:250'])
+                ->examples(['SN123456789', 'SN987654321']),
 
             ImportColumn::make('weight')
                 ->label(__('general.weight'))
-                ->rules(['nullable', 'max:250']),
+                ->rules(['nullable', 'max:250'])
+                ->examples(['500g', '45kg']),
 
             ImportColumn::make('manufacturer_barcode')
                 ->label(__('general.manufacturer_barcode'))
-                ->rules(['nullable', 'max:255']),
+                ->rules(['nullable', 'max:255'])
+                ->examples(['4000123456789', '7612345678901']),
 
             ImportColumn::make('url')
                 ->label(__('general.url'))
-                ->rules(['nullable', 'url']),
+                ->rules(['nullable', 'url'])
+                ->examples(['https://example.com/produkt1', 'https://example.com/produkt2']),
 
             ImportColumn::make('due_date')
                 ->label(__('general.due_date'))
-                ->rules(['nullable', 'date']),
+                ->rules(['nullable', 'date'])
+                ->examples([now()->addYear()->format('Y-m-d'), '2025-12-31']),
 
             ImportColumn::make('buy_date')
                 ->label(__('general.buy_date'))
-                ->rules(['nullable', 'date']),
+                ->rules(['nullable', 'date'])
+                ->examples([now()->format('Y-m-d'), '2024-01-01']),
 
             ImportColumn::make('owner')
                 ->label(__('general.owner'))
-                ->rules(['nullable', 'max:10000']),
+                ->rules(['nullable', 'max:10000'])
+                ->examples(['Max Mustermann', 'Logistik Abteilung']),
 
             // --- Tab: More / Notes (Boolean Flags) ---
             ImportColumn::make('dangerous_good')
                 ->label(__('general.dangerous_good'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['0', '1']),
 
             ImportColumn::make('big_size')
                 ->label(__('general.big_size'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['0', '1']),
 
             ImportColumn::make('needs_truck')
                 ->label(__('general.needs_truck'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['0', '1']),
 
             ImportColumn::make('stackable')
                 ->label(__('general.stackable'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['1', '0']),
 
             ImportColumn::make('borrowed_item')
                 ->label(__('general.borrowed_item'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['0', '1']),
 
             ImportColumn::make('rented_item')
                 ->label(__('general.rented_item'))
                 ->boolean()
-                ->rules(['boolean']),
+                ->rules(['boolean'])
+                ->examples(['0', '1']),
         ];
     }
 
@@ -119,10 +138,11 @@ class ItemImporter extends Importer
             Select::make('department_id')
                 ->label(__('general.department'))
                 ->options(function () {
-                    $user = Auth::user();
-
-                    #TODO: Only accessible departments should be listed here. This is currently not the case
-                    return $user->departments()->pluck('name', 'departments.id');
+                    if (Auth::user()->can('can-create-items-for-other-departments')) {
+                        return Department::all()->pluck('name', 'id')->toArray();
+                    } else {
+                        return Auth::user()->getDepartmentsWithPermission('create-Item')->pluck('name', 'id')->toArray();
+                    }
                 })
                 ->required(),
 
