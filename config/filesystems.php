76c6e41af9ed3fdbd71d537d7aa36d308376bff9
Lumex <54485140+LumexDE@users.noreply.github.com>
2026-09-17 16:33:13 +0200

    chore: Add Hetzner S3 configuration to filesystems and `.env.example`

--- config/filesystems.php
+++ config/filesystems.php
@@ -75,6 +75,18 @@ return [
             'timeout' => 20,
         ],
 
+        'hetzner' => [
+            'driver' => 's3',
+            'key' => env('HETZNER_S3_ACCESS_KEY_ID'),
+            'secret' => env('HETZNER_S3_SECRET_ACCESS_KEY'),
+            'region' => 'eu-central',
+            'bucket' => env('HETZNER_S3_BUCKET'),
+            'endpoint' => env('HETZNER_S3_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+            'visibility' => 'private',
+        ],
+
     ],
 
     /*
